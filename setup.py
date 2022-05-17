7f3f9f445d4feff2d04776b1eb9bbc915d8b3a26
Jason Yunger <jason.yunger@gmail.com>
2022-05-17 08:11:29 -0400

    getting started

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='wutest',
+    version='0.0.1',
+    author='Jason Yunger',
+    author_email='jason.yunger@gmail.com',
+    description='Testing installation of Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/jyunger/wutest',
+    project_urls = {
+        "Bug Tracker": "https://github.com/jyunger/wutest/issues"
+    },
+    license='MIT',
+    packages=['wutools'],
+    install_requires=['numpy', 'pandas', 'psutils'],
+)
