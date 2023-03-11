c2bec64ffb7891631517a06149d9a90b5e2d7fa5
Jason Yunger <jason.yunger@gmail.com>
2023-03-11 15:29:01 -0500

    added ml subpackage

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setuptools.setup(
         "Bug Tracker": "https://github.com/jyunger/wutest/issues"
     },
     license='MIT',
-    packages=['wutools'],
+    packages=['wutools', 'wutools.ml'],
     install_requires=['numpy', 'pandas', 'psutil', 'pyathena'],
 )
