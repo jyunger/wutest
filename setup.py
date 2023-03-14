90e48b74f40626dc2e42878fc75ffd47d41936ea
Jason Yunger <jason.yunger@gmail.com>
2023-03-14 09:24:38 -0400

    Relocated data in the package. Now can access the course hash migration as a dict in wutools.data.migration

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setuptools.setup(
         "Bug Tracker": "https://github.com/jyunger/wutest/issues"
     },
     license='MIT',
-    packages=['wutools', 'wutools.ml', 'wutools.data'],
+    packages=setuptools.findpackages(),
     install_requires=['numpy', 'pandas', 'psutil', 'pyathena', 'graphviz'],
 )
